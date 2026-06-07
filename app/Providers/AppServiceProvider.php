f35731c2c7697e4e8f2b4908c1426087b9737975
Damien Mauchamp <damienmauchamp@gmail.com>
2026-06-06 20:52:09 -0400

    feat(app): refactor application setup and configuration for improved routing and middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Modules\Album\Models\Album;
@@ -26,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
         Schema::defaultStringLength(191);
 
         // Model::shouldBeStrict(! app()->isProduction());
@@ -43,5 +43,9 @@ class AppServiceProvider extends ServiceProvider
         Request::macro('artist', fn () => $this->get('artist')
             ?? Artist::where('id', $this->header('artist'))->first()
         );
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
